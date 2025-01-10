b3a42229130c03fa1ef04e8ef95351123eae8d07
blairecas <chaminealex@mail.ru>
2025-01-10 22:35:11 +0300

    test menestrel

--- 7_compile_tstay.bat
+++ 7_compile_tstay.bat
@@ -4,18 +4,18 @@ echo.
 echo ===========================================================================
 echo Compiling
 echo ===========================================================================
-php -f ../scripts/preprocess.php bunive.mac
+php -f ../scripts/preprocess.php tstay.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\scripts\macro11.exe -ysl 32 -yus -l _bunive.lst _bunive.mac
+..\scripts\macro11.exe -ysl 32 -yus -l _tstay.lst _tstay.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-php -f ../scripts/lst2bin.php _bunive.lst ./release/bunive.bin bbk 2000
+php -f ../scripts/lst2bin.php _tstay.lst ./release/tstay.bin bbk 2000
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-php -f ../scripts/bin2wav.php ./release/bunive.bin
+php -f ../scripts/bin2wav.php ./release/tstay.bin
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 
-del _bunive.mac
-del _bunive.lst
+del _tstay.mac
+del _tstay.lst
 
-start ..\..\bkemu\BK_x64.exe /C BK-0010-01 /B .\release\bunive.bin
+start ..\..\bkemu\BK_x64.exe /C BK-0011M /B .\release\tstay.bin
 
 echo.
--- 8_compile_tstmen.bat
+++ 8_compile_tstmen.bat
@@ -4,18 +4,18 @@ echo.
 echo ===========================================================================
 echo Compiling
 echo ===========================================================================
-php -f ../scripts/preprocess.php bunive.mac
+php -f ../scripts/preprocess.php tstmen.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\scripts\macro11.exe -ysl 32 -yus -l _bunive.lst _bunive.mac
+..\scripts\macro11.exe -ysl 32 -yus -l _tstmen.lst _tstmen.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-php -f ../scripts/lst2bin.php _bunive.lst ./release/bunive.bin bbk 2000
+php -f ../scripts/lst2bin.php _tstmen.lst ./release/tstmen.bin bbk 2000
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-php -f ../scripts/bin2wav.php ./release/bunive.bin
+php -f ../scripts/bin2wav.php ./release/tstmen.bin
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 
-del _bunive.mac
-del _bunive.lst
+del _tstmen.mac
+del _tstmen.lst
 
-start ..\..\bkemu\BK_x64.exe /C BK-0010-01 /B .\release\bunive.bin
+start ..\..\bkemu\BK_x64.exe /C BK-0010-01 /B .\release\tstmen.bin
 
 echo.
