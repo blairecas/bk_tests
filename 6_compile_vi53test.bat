28568e9903e7628cdebce7d51d690572022063dc
blairecas <chaminealex@mail.ru>
2025-02-19 09:18:25 +0300

    mode2 test

---
+++ 6_compile_vi53test.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo.
+echo ===========================================================================
+echo Compiling
+echo ===========================================================================
+php -f ../scripts/preprocess.php vi53test.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+..\scripts\macro11.exe -ysl 32 -yus -l _vi53test.lst _vi53test.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f ../scripts/lst2bin.php _vi53test.lst ./release/vi53test.bin bkpack
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f ../scripts/bin2wav.php ./release/vi53test.bin
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+
+del _vi53test.mac
+del _vi53test.lst
+
+..\scripts\bkdecmd d ./release/andos.img vi53test >NUL
+..\scripts\bkdecmd a ./release/andos.img ./release/vi53test.bin >NUL
+
+start ..\..\bkemu\BK_x64.exe /C BK-0011M /B .\release\vi53test.bin
+
+echo.
