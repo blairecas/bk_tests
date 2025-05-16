38eda407f40785ceb4f7d5a192e4d4d0770fe996
blairecas <chaminealex@mail.ru>
2025-05-17 00:55:02 +0300

    pt3mus

---
+++ 10_compile_pt3mus.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set NAME=pt3mus
+
+echo.
+echo ===========================================================================
+echo Compiling
+echo ===========================================================================
+php -f ../scripts/preprocess.php %NAME%.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+..\scripts\macro11.exe -ysl 32 -yus -l _%NAME%.lst _%NAME%.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f ../scripts/lst2bin.php _%NAME%.lst ./release/%NAME%.bin bkpack
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+
+del _%NAME%.mac
+del _%NAME%.lst
+
+..\scripts\bkdecmd d ./release/andos_pt3mus.img %NAME% >NUL
+..\scripts\bkdecmd a ./release/andos_pt3mus.img ./release/%NAME%.bin >NUL
+
+..\scripts\bkdecmd d ./release/andos_pt3mus.img PIC001.BIN >NUL
+..\scripts\bkdecmd a ./release/andos_pt3mus.img ./graphics/PIC001.BIN >NUL
+..\scripts\bkdecmd d ./release/andos_pt3mus.img MUS001.PT3 >NUL
+..\scripts\bkdecmd a ./release/andos_pt3mus.img ./music/MUS001.PT3 >NUL
+
+echo.
